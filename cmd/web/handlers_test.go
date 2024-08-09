ab0d68b5474bb634f508ece1f16f38296fbbe928
victor <victorreisprog@gmail.com>
2024-08-09 11:43:43 -0300

    feat: test handlers app home

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -36,4 +39,42 @@ func Test_application_handlers(t *testing.T) {
 			t.Errorf("for %s: expected %d; got %d", tt.name, tt.expectedStatusCode, resp.StatusCode)
 		}
 	}
+}
+
+func TestAppHomeOld(t *testing.T) {
+	// criar a request
+	req, _ := http.NewRequest("GET", "/", nil)
+
+	req = addContextAndSessionToRequest(req, app)
+
+	// criando um response writer
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(app.Home)
+
+	handler.ServeHTTP(rr, req)
+
+	// checando o status code
+	if rr.Code != http.StatusOK {
+		t.Errorf("TestAppHome return wrong status code: got %v, want %v", rr.Code, http.StatusOK)
+	}
+
+	body, _ := io.ReadAll(rr.Body)
+	want := string("<small>From Session:")
+	if !strings.Contains(string(body), want) {
+		t.Errorf("TestAppHome: body does not contain %s", want)
+	}
+}
+
+func getCtx(req *http.Request) context.Context {
+	ctx := context.WithValue(req.Context(), contextUserKey, "unknown")
+	return ctx
+}
+
+func addContextAndSessionToRequest(req *http.Request, app application) *http.Request {
+	req = req.WithContext(getCtx(req))
+	// o header X-Session é esperado que esteja presente na request se formos usar a session
+	ctx, _ := app.Session.Load(req.Context(), req.Header.Get("X-Session"))
+
+	return req.WithContext(ctx)
 }
